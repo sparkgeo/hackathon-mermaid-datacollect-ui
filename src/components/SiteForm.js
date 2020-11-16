bc8de765ec2adf317692389cc7d087f6abcc248b
Brian Bancroft <hello@brianbancroft.ca>
2020-11-16 11:17:35 -0800

    💄  Add additional elements to the form

--- src/components/SiteForm.js
+++ src/components/SiteForm.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Box, Text, FormField, Form, TextInput } from 'grommet'
+import { Box, Text, FormField, Form, TextInput, Heading } from 'grommet'
+import { MapContainer, Marker, TileLayer } from 'react-leaflet'
 
 function SiteForm() {
   return (
@@ -7,7 +8,6 @@ function SiteForm() {
       margin="medium"
       border={{ size: 'xsmall', color: 'dark-3' }}
       width="xxlarge"
-      height="xxlarge"
       direction="column"
     >
       <Form>
@@ -28,14 +28,49 @@ function SiteForm() {
               <FormField label="Country" required>
                 <TextInput />
               </FormField>
+
               <FormField label="Latitude" required>
                 <TextInput />
               </FormField>
+              <Text size="small" color="dark-4">
+                Decimal Degrees
+              </Text>
+              <br />
               <FormField label="Longitude" required>
                 <TextInput />
               </FormField>
+              <Text size="small" color="dark-4">
+                Decimal Degrees
+              </Text>
+              <br />
             </Box>
-            <Box>Map</Box>
+            <Box>
+              <Heading>MAP</Heading>
+              {/* <MapContainer center={[0, 0]} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[0, 0]}></Marker>
+              </MapContainer> */}
+            </Box>
+          </Box>
+          <Box margin="small">
+            <FormField label="Exposure" required>
+              <TextInput />
+            </FormField>
+            <FormField label="Reef Type" required>
+              <TextInput />
+            </FormField>
+            <FormField label="Reef Zone" required>
+              <TextInput />
+            </FormField>
+          </Box>
+          <hr />
+          <Box margin="small">
+            <FormField label="Notes" required>
+              <TextInput />
+            </FormField>
           </Box>
         </Box>
       </Form>
