f08c43ee8f5c6a0df12d1ad22aefd1d57d13d97a
Brian Bancroft <hello@brianbancroft.ca>
2020-11-17 14:12:09 -0800

    Clean breadcrumbs

--- src/components/Breadcrumbs.js
+++ src/components/Breadcrumbs.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import { Box, Anchor, Text } from 'grommet'
 import { FormNext } from 'grommet-icons'
+import { useLocation } from 'react-router-dom'
 
 const Breadcrumbs = () => {
+  console.log(window.location.href)
+  const { pathname } = window.location
+
+  const newRecord = /new/.test(pathname)
+  const viewRecord = /record\.*/.test(pathname)
+  const editRecord = /edit/.test(pathname)
+
   return (
     <Box pad={{ top: 'medium' }}>
       <Box height="xxsmall" direction="row" align="center">
@@ -10,9 +18,26 @@ const Breadcrumbs = () => {
         <FormNext />
         <Anchor href="#">Dustin's Project</Anchor>
         <FormNext />
-        <Anchor href="#">Sites</Anchor>
-        <FormNext />
-        <Text>Site</Text>
+        <Anchor href="/">Site List</Anchor>
+
+        {newRecord && (
+          <>
+            <FormNext />
+            <Anchor href="#">New</Anchor>
+          </>
+        )}
+        {viewRecord && (
+          <>
+            <FormNext />
+            <Anchor href="#">Site</Anchor>
+          </>
+        )}
+        {editRecord && (
+          <>
+            <FormNext />
+            <Anchor href="#">edit</Anchor>
+          </>
+        )}
       </Box>{' '}
     </Box>
   )
