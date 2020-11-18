d3af4e44fc8a8a398a30c42664e78ab7544b5a2a
Brian Bancroft <hello@brianbancroft.ca>
2020-11-18 09:05:46 -0800

    ✨  Improve initial load

--- src/routes/HomePage.js
+++ src/routes/HomePage.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from 'react'
 
+import { Hub } from '@aws-amplify/core'
+import { DataStore, Predicates } from '@aws-amplify/datastore'
+import { Site } from '../models'
+
 import { retrieveAllRecords } from '../lib/api'
 import TableSites from '../components/TableSites'
 
@@ -9,17 +13,30 @@ export default function HomePage() {
   const [error, setError] = useState('')
 
   useEffect(() => {
-    const retrieveRecords = async () => {
-      const data = await retrieveAllRecords().catch((e) => {
-        console.error(e)
-        setLoading(false)
-        setError('API Loading Error')
-      })
+    // Create listener that will stop observing the model once the sync process is done
+    const removeListener = Hub.listen('datastore', async (capsule) => {
+      const {
+        payload: { event, data },
+      } = capsule
 
-      setRecords(data ?? [])
-      setLoading(false)
+      console.log('DataStore event', event, data)
+
+      if (event === 'ready') {
+        const sites = await DataStore.query(Site, Predicates.ALL, {
+          // page: 0,
+          // limit: 15,
+        })
+        setLoading(false)
+        setRecords(sites)
+      }
+    })
+
+    // Start the DataStore, this kicks-off the sync process.
+    DataStore.start()
+
+    return () => {
+      removeListener()
     }
-    retrieveRecords()
   }, [])
 
   if (loading) return <h1>loading</h1>
