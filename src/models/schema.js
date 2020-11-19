86328c1deade0c963a5b2eda02fd9a33f55bd763
Parmvir Thind <parmvirsthind@gmail.com>
2020-11-19 12:07:39 -0800

    [🔊 AMPLIFY ] update schema and models

--- src/models/schema.js
+++ src/models/schema.js
@@ -1,90 +1,134 @@
 export const schema = {
-  models: {
-    Site: {
-      name: 'Site',
-      fields: {
-        id: {
-          name: 'id',
-          isArray: false,
-          type: 'ID',
-          isRequired: true,
-          attributes: [],
-        },
-        name: {
-          name: 'name',
-          isArray: false,
-          type: 'String',
-          isRequired: true,
-          attributes: [],
-        },
-        country: {
-          name: 'country',
-          isArray: false,
-          type: 'String',
-          isRequired: true,
-          attributes: [],
-        },
-        latitude: {
-          name: 'latitude',
-          isArray: false,
-          type: 'Float',
-          isRequired: true,
-          attributes: [],
-        },
-        longitude: {
-          name: 'longitude',
-          isArray: false,
-          type: 'Float',
-          isRequired: true,
-          attributes: [],
-        },
-        exposure: {
-          name: 'exposure',
-          isArray: false,
-          type: 'String',
-          isRequired: true,
-          attributes: [],
-        },
-        reefType: {
-          name: 'reefType',
-          isArray: false,
-          type: 'String',
-          isRequired: true,
-          attributes: [],
-        },
-        reefZone: {
-          name: 'reefZone',
-          isArray: false,
-          type: 'String',
-          isRequired: true,
-          attributes: [],
-        },
-        notes: {
-          name: 'notes',
-          isArray: false,
-          type: 'String',
-          isRequired: false,
-          attributes: [],
-        },
-        createdAt: {
-          name: 'createdAt',
-          isArray: false,
-          type: 'String',
-          isRequired: false,
-          attributes: [],
-        },
-      },
-      syncable: true,
-      pluralName: 'Sites',
-      attributes: [
-        {
-          type: 'model',
-          properties: {},
-        },
-      ],
+    "models": {
+        "Site": {
+            "name": "Site",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "name": {
+                    "name": "name",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "country": {
+                    "name": "country",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "latitude": {
+                    "name": "latitude",
+                    "isArray": false,
+                    "type": "Float",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "longitude": {
+                    "name": "longitude",
+                    "isArray": false,
+                    "type": "Float",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "exposure": {
+                    "name": "exposure",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "reefType": {
+                    "name": "reefType",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "reefZone": {
+                    "name": "reefZone",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "notes": {
+                    "name": "notes",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": false,
+                    "attributes": []
+                },
+                "createdAt": {
+                    "name": "createdAt",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": false,
+                    "attributes": []
+                },
+                "siteStatus": {
+                    "name": "siteStatus",
+                    "isArray": true,
+                    "type": {
+                        "nonModel": "Status"
+                    },
+                    "isRequired": false,
+                    "attributes": [],
+                    "isArrayNullable": true
+                }
+            },
+            "syncable": true,
+            "pluralName": "Sites",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                }
+            ]
+        }
     },
-  },
-  enums: {},
-  nonModels: {},
-  version: '2bc8a1d98613666ba569946e3a2a230c',
-}
+    "enums": {},
+    "nonModels": {
+        "Status": {
+            "name": "Status",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "user": {
+                    "name": "user",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "field": {
+                    "name": "field",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "time": {
+                    "name": "time",
+                    "isArray": false,
+                    "type": "Int",
+                    "isRequired": true,
+                    "attributes": []
+                }
+            }
+        }
+    },
+    "version": "b702df5ce91650e0bb6193260148d4a4"
+};
