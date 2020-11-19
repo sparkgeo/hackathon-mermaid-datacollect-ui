43ed901b5adcc15d1614294c5cb83b920366a4cf
Brian Bancroft <hello@brianbancroft.ca>
2020-11-19 10:33:19 -0800

    🏗   Overhaul pwa setup
    In response to upgrading the react-scripts, their built-in
    pwa helper was changed and the original one no longer worked.
    
    This resolves that particular turkey

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import * as serviceWorker from './serviceWorker'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import Amplify from '@aws-amplify/core'
 import awsConfig from './aws-exports'
+import reportWebVitals from './reportWebVitals'
 
 if (process.env.REACT_APP_API_MODE === 'amplify') Amplify.configure(awsConfig)
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register()
+serviceWorkerRegistration.register()
+reportWebVitals()
