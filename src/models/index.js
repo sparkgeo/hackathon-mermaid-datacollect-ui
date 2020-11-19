f2e70b6ff43c9c25807ee50f944128cd737654a4
Alain St. Pierre <alain@sparkgeo.com>
2020-11-18 19:23:34 -0800

    prettier

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,7 @@
 // @ts-check
-import { initSchema } from '@aws-amplify/datastore';
-import { schema } from './schema';
+import { initSchema } from '@aws-amplify/datastore'
+import { schema } from './schema'
 
+const { Site } = initSchema(schema)
 
-
-const { Site } = initSchema(schema);
-
-export {
-  Site
-};
+export { Site }
